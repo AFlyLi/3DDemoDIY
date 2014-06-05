da195970cece9263157c89df019e1be0ae241d7f
沈海鹏 <451150983@qq.com>
2014-06-05 15:40:19 +0800

    Initial Commit

---
+++ PictureDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PictureDemo
+//
+//  Created by 沈海鹏 on 14-6-5.
+//  Copyright (c) 2014年 shenhaipeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PictureDemoTests/PictureDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  PictureDemoTests.m
+//  PictureDemoTests
+//
+//  Created by 沈海鹏 on 14-6-5.
+//  Copyright (c) 2014年 shenhaipeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PictureDemoTests : XCTestCase
+
+@end
+
+@implementation PictureDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
